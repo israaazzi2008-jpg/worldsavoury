9fa923bd595b2a5d4ca3d1fd0c760ded014af50b
israaazzi2008-jpg <israa.azzi2008@gmail.com>
2026-06-16 01:28:33 +0200

    Fix imagePlaceholder path for cake2

--- src/App.tsx
+++ src/App.tsx
@@ -74,7 +74,7 @@ const MENU_ITEMS: MenuItem[] = [
     name: "Gâteau Cake de Rêve ",
     category: "Cakes",
     description: "24cm sur 24cm pour 12 personnes",
-    imagePlaceholder: "/tart22.jpg"
+    imagePlaceholder: "/tarte2.jpg"
   },
   {
     id: 'cake3',
